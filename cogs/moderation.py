3be64db330f0de9178165f7d64bd56780703ec77
The-Mystery <42754166+the-mystery@users.noreply.github.com>
2019-08-28 14:38:41 +1200

    Created moderation.py file
    Signed-off-by: TheMystery <the-mystery@outlook.com>

---
+++ cogs/moderation.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+import discord
+from .utils import time
+import logging
+import json
+import datetime
+import asyncio
+
+
+class Moderation(commands.Cog):
+    '''Moderation Commands'''
+    def __init__(self, bot):
+        self.bot = bot
+
+
+
+
+def setup(bot)
+    bot.add_bot(Moderation(bot))
